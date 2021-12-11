f1869bd9e5d0a1319a3506d6859c3ea9f01641fc
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2021-12-11 12:23:33 +0200

    befor edited in localStorage

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const images = [    // array of all photo with all properities
+const images = [
+  // array of all photo with all properities
   {
     src: "./animal.jpg",
     alt: "animal picture",
@@ -222,15 +223,12 @@ const addToDiv = (array, divName) => {
       $(`<svg style="color: #013033;" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-suit-heart-fill" viewBox="0 0 16 16">
       <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"/>
     </svg>`);
-    
+
     favorite.on("click", () => {
       myFav.push(element);
-       localStorage.setItem("src", element.src);
-       localStorage.setItem("alt", element.alt);
-       localStorage.setItem("id", element.id);
-       localStorage.setItem("title", element.title);
-       localStorage.setItem("disc", element.discription);
-      favorite.attr('style',"color:red;")
+      localStorage.setItem("favorites", JSON.stringify(myFav));
+      console.log(JSON.parse(localStorage.getItem("favorites")));
+      favorite.attr("style", "color:red;");
     });
     disc.text(element.discription);
     image.appendTo(card);
@@ -251,12 +249,11 @@ const favList = $('<div class="Favorite"></div>');
 const fav = $('<button class="fav">Favorite</button>');
 favList.appendTo(navBar);
 fav.on("click", () => {
- 
   // myFav = localStorage.getItem("myFav");
-  
-  $('.image').remove();
+
+  $(".image").remove();
   addToDiv(myFav, myfav);
-  $('.favorite').attr('style',"color:red;")
+  $(".favorite").attr("style", "color:red;");
 });
 fav.appendTo(favList);
 
@@ -267,26 +264,27 @@ const cat = $('<div class="cat"></div>');
 categoires.appendTo(navBar);
 list.appendTo(categoires);
 cat.appendTo(list);
-const items = [   //array of cetagories
+const items = [
+  //array of cetagories
   { name: "All", id: 0 },
   { name: "Animal", id: 1 },
   { name: "Space", id: 2 },
   { name: "Nature", id: 3 },
   { name: "Cartoon", id: 4 },
-  { name: "Tourism", id: 5 }
+  { name: "Tourism", id: 5 },
 ];
 items.forEach((element) => {
   const item = $("<h4></h4>");
   item.text(element.name);
   item.on("click", () => {
-    if (element.id == 0){
-      $('.image').remove();
+    if (element.id == 0) {
+      $(".image").remove();
       addToDiv(images, cards);
-    }else{
+    } else {
       const newImages = images.filter((ele) => {
         return element.id == ele.id;
       });
-      $('.image').remove();
+      $(".image").remove();
       addToDiv(newImages, filtered);
     }
   });
